0c10c4b05b75d9cdb02dca16f06279506bae5002
shadowxmd2-cyber <shadowxmd2@gmail.com>
2026-02-02 18:19:14 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,20 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } 
-from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import {
+ signInWithEmailAndPassword,
+ createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 window.login=()=>{
  signInWithEmailAndPassword(auth,email.value,password.value)
- .then(()=>location.href="index.html")
+ .then(()=>{
+  loginBox.style.display="none";
+  goBox.style.display="block";
+ })
  .catch(e=>alert(e.message));
 };
 
 window.register=()=>{
  createUserWithEmailAndPassword(auth,email.value,password.value)
- .then(()=>alert("Registered"))
+ .then(()=>alert("Registered Successfully"))
  .catch(e=>alert(e.message));
 };
